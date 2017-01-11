9d5edce13243e4372c23b363f0c003c74063e4bb
renhengli <renhengli@renhengli>
2017-01-11 18:41:24 +0800

    First commit

---
+++ src/main/java/com/renhengli/Application.java
@@ -0,0 +1,18 @@
+package com.renhengli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 2017-1-11
+ * 
+ * @author renhengli
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/main/java/com/renhengli/controller/User1Controller.java
@@ -0,0 +1,42 @@
+package com.renhengli.controller;
+
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.renhengli.mapper.UserMapper;
+
+/**
+ * 
+ * @author renhengli
+ *
+ */
+@Controller
+@RequestMapping("/home")
+public class User1Controller {
+	private static Logger logger = Logger.getLogger(User1Controller.class);
+	
+	@Autowired
+	UserMapper userMapper;
+
+	// 从 application.properties 中读取配置，如取不到默认值为Hello Shanhy
+	@Value("${application.hello:Hello Angel}")
+	private String hello;
+	
+
+	@RequestMapping("/user1")
+	public String user1(Map<String, Object> map) {
+		logger.info("-----start-------");
+		logger.debug("-----debug log-------");
+		logger.error("-----error log-------");
+		System.out.println("UserController.user1().hello=" + hello);
+		map.put("hello", hello);
+		logger.info("-----end---------");
+		return "User";
+	}
+}
